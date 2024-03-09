6dcd6faca67d495be9e830b79560edeb381413dd
OlVin69 <aviniichuk@gmail.com>
2024-03-09 05:03:04 +0200

    add all files

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,47 +1,86 @@
+import toast, { Toaster } from 'react-hot-toast';
+import { useState, useEffect } from 'react';
+import ImageGallery from '../ImageGallery/ImageGallery';
+import SearchBar from '../SearchBar/SearchBar';
+import MyLoader from '../MyLoader/MyLoader';
+import ErrorMessage  from '../ErrorMessage/ErrorMessage';
+import ImageModal from '../imageModal';
+import { fetchPhotos } from '../../photos-api';
 import './App.css';
 
-import { useState, useEffect } from 'react';
-import ContactForm from './ContactForm/ContactForm';
-import ContactList from './ContactList/ContactList ';
-import SearchBox from './SearchBox/SearchBox';
-
 export default function App() {
-  const [contacts, setContacts] = useState(() => {
-    const savedContacts = localStorage.getItem('saved-contacts');
-    if (savedContacts) {
-      const parsedContacts = JSON.parse(savedContacts);
-      if (Array.isArray(parsedContacts) && parsedContacts.length > 0) {
-        return parsedContacts;
+
+  const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
+  const [photos, setPhotos] = useState([])
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedImg, setSelectedImg] = useState("");
+  const [totalPages, setTotalPages] = useState(null)
+
+  useEffect(()=> {
+    async function getData() {
+
+      if(query === ""){
+        return;
+      }
+
+      try {
+        setLoading(true)
+        setError(false)
+        const data = await fetchPhotos(query, page);
+        setPhotos((prevPhotos)=>{
+          return[...prevPhotos, ...data[0]]
+        }); 
+        
+        setTotalPages(data[1]);
+
+         if (data[0].length === 0) {
+          toast.error('Unfortunately, there is no image for your request')
+         }
+
+      } catch (error) {
+        setError(true);
+        toast.error("WARNING")
+      }
+      finally{
+        setLoading(false);
       }
     }
-    return [];
-  });
-  const [filter, setFilter] = useState('');
 
-  const addContact = newContact => {
-    setContacts(prevContacts => [...prevContacts, newContact]);
-  };
+    getData();
+  }, [query, page, ]);
+   
 
-  const deleteContact = contactId => {
-    setContacts(prevContacts => {
-      return prevContacts.filter(contact => contact.id !== contactId);
-    });
-  };
+  const handleSearch =  (topic) =>{
+    setQuery(topic);
+    setPage(1);
+    setPhotos([]); 
+  }
 
-  useEffect(() => {
-    localStorage.setItem('saved-contacts', JSON.stringify(contacts));
-  }, [contacts]);
+  const handleLoadMore =()=>{
+    setPage(page+1)
+  }
 
-  const visibleContacts = contacts.filter(({ name }) =>
-    name.toLowerCase().includes(filter.toLowerCase())
-  );
-
+  function closeModal() {
+    setModalIsOpen(false);
+  }
+  
+  const handleImgClick = (imageUrl) => {
+    setSelectedImg(imageUrl);
+    setModalIsOpen(true);
+  }
+  
   return (
-    <div>
-      <h1>Phonebook</h1>
-      <ContactForm onAdd={addContact} />
-      <SearchBox value={filter} onFilter={setFilter} />
-      <ContactList contacts={visibleContacts} onDelete={deleteContact} />
-    </div>
-  );
+  <div>
+    <SearchBar onSearch={handleSearch}/>
+    {error && <ErrorMessage/>}
+    {photos.length > 0 && <ImageGallery items={photos} onImageClick={handleImgClick} />}
+    {photos.length > 0 && !loading && totalPages && totalPages !== page && <button className='loadmore' type='button' onClick={handleLoadMore}>Load more</button>}
+    {loading && <MyLoader/>}
+    <ImageModal onRequestClose = {closeModal} isOpen={modalIsOpen} imageUrl={selectedImg}/>
+    
+    <Toaster/>
+  </div>);
 }
